940f1b92f101f3925a852fe31a1c4e480be42d1a
riinavi <riinavi86@gmail.com>
2022-07-07 23:33:33 +0300

    fetch data asynchronously

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { RecoilRoot } from 'recoil';
+import {
+  atom,
+  selector,
+  useRecoilValueLoadable,
+} from 'recoil';
 
-import CharacterCounter from './components/CharacterCounter';
+const getData = async () => {
+  return fetch('https://catfact.ninja/facts')
+    .then((response) => response.json())
+    .then(({ data }) => data);
+}
+
+const catFactsState = atom<[]>({
+  key: 'catFacts',
+  default: selector({
+    key: 'catFactsLoader',
+    get: async () => {
+      return await getData()
+    },
+  }),
+})
+
 
 function App() {
-  return (
-    <RecoilRoot>
-      Characters Counter
+  const data = useRecoilValueLoadable(catFactsState);
 
-      <CharacterCounter />
-    </RecoilRoot>
-  );
+  if (data.state === "hasError") {
+    return <div> There is some problem! </div>;
+  }
+
+  if (data.state === "loading") {
+    return <div>Loading data...</div>;
+  }
+
+  if (data.state === "hasValue") {
+    return (
+      <div>{data.contents.map((el: { fact: string }, i: number) => (
+        <p key={i}>{el.fact}</p>
+      ))}</div>
+    );
+  }
+
+  return <>No data</>
 }
 
 export default App;
